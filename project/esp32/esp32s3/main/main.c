0a0b88fe70dae1ba67ce1d5a754e118f16b0f102
daniel.zhang <daniel.zhang@fulicare.com>
2023-02-17 18:30:52 +0800

    修改main.c文件

--- project/esp32/esp32s3/main/main.c
+++ project/esp32/esp32s3/main/main.c
@@ -4,15 +4,18 @@
 #include "freertos/task.h"
 
 #include "zlog_demo.h"
+#include "ux_demo.h"
 
 TaskHandle_t g_task1_handle;
 
 static void task1_handle(void *arg)
 {
     zlog_demo_init();
+    ux_demo_init();
+
     while (1) {
-        vTaskDelay(pdMS_TO_TICKS(3000));
-        printf("this is task1\r\n");
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        // printf("this is task1\r\n");
     }
     
 }
